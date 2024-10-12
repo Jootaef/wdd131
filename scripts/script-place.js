bc51abb95a190d5af0bfe83faa6929e5e5e1ba02
jootaef <jaydanv1008@gmail.com>
2024-10-11 23:41:56 -0500

    1

--- scripts/script-place.js
+++ scripts/script-place.js
@@ -1,22 +1,18 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  
+    document.getElementById('currentYear').textContent = new Date().getFullYear();
+    document.getElementById('lastModified').textContent = document.lastModified;
 
-function calculateWindChill(temp, windSpeed) {
-    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16)).toFixed(2);
-}
+    const temperature = 18; 
+    const windSpeed = 10; 
+    const condition = "Sunny"; 
 
-document.getElementById('currentYear').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = document.lastModified;
+    document.getElementById('condition').textContent = condition;
 
-
-const temperature = 18; 
-const windSpeed = 10; 
-const condition = "Sunny"; 
-
-document.getElementById('condition').textContent = condition;
-
-
-if (temperature <= 10 && windSpeed > 4.8) {
-    const windChill = calculateWindChill(temperature, windSpeed);
-    document.getElementById('windChill').textContent = `${windChill} °C`;
-} else {
-    document.getElementById('windChill').textContent = 'N/A';
-}
+    if (temperature <= 10 && windSpeed > 4.8) {
+        const windChill = calculateWindChill(temperature, windSpeed);
+        document.getElementById('windChill').textContent = `${windChill} °C`;
+    } else {
+        document.getElementById('windChill').textContent = 'N/A';
+    }
+});
